23606c6037f86e95aff499bcf77bf6df4663841e
ovictorqueiroz <victorbr442@gmail.com>
2025-10-16 22:19:31 -0300

    Iniciando Tela de cadastro

--- app/src/main/java/br/com/etecia/myapp/CadastroUserActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastroUserActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CadastroUserActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class CadastroUserActivity extends AppCompatActivity {
+    MaterialButton btnCadastrar;
+
+    TextView txtEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,16 @@ public class CadastroUserActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtEntrar = findViewById(R.id.txtEntrar);
+
+        txtEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
         });
     }
 }
