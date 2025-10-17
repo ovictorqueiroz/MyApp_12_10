c4e4e403da4d5ece3549dfc65e81323e5e6c3d44
ovictorqueiroz <victorbr442@gmail.com>
2025-10-16 21:56:48 -0300

    Integrando Login e outras telas

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActivity extends AppCompatActivity {
+    MaterialButton btnEntrar;
+    TextView txtRecPass;
+    TextView txtCadastrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,26 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        txtCadastrar = findViewById(R.id.txtCadastrar);
+        txtRecPass = findViewById(R.id.txtRecPass);
+
+        txtCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CadastroUserActivity.class));
+                finish();
+            }
+        });
+        txtRecPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RecuperarUsuarioActivity.class));
+                finish();
+            }
+        });
+
+
     }
 }
