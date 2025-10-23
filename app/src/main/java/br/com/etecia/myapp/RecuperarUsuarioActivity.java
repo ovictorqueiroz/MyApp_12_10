cb67936fa29ba3cfb8e86cf7fd9768b91299f8e4
ovictorqueiroz <victorbr442@gmail.com>
2025-10-23 20:13:01 -0300

    feat(recSenha): Interligando tela de login e recuperar senha

--- app/src/main/java/br/com/etecia/myapp/RecuperarUsuarioActivity.java
+++ app/src/main/java/br/com/etecia/myapp/RecuperarUsuarioActivity.java
@@ -1,6 +1,10 @@
 package br.com.etecia.myapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RecuperarUsuarioActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class RecuperarUsuarioActivity extends AppCompatActivity {
+    TextView txtEntrar;
+    MaterialButton btnRecPass;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,17 @@ public class RecuperarUsuarioActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnRecPass = findViewById(R.id.btnRecPass);
+        txtEntrar = findViewById(R.id.txtEntrar);
+
+        txtEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
     }
 }
