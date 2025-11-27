e6e76196aad7f7bbde39c54f941110e4843134b7
ovictorqueiroz <victorbr442@gmail.com>
2025-11-27 20:20:34 -0300

    POO em Android

---
+++ app/src/main/java/br/com/etecia/myapp/ListBicicletas.java
@@ -0,0 +1,12 @@
+package br.com.etecia.myapp;
+
+public class ListBicicletas {
+    private String tituloBike;
+    private String descBike;
+    private float precoBike;
+    private int imgBike;
+
+    public ListBicicletas() {
+    }
+
+}
